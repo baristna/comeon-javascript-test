3deb67ecf3657430f3a4640bccf914d31d0d5de1
Baris Tuna <baristna@gmail.com>
2020-09-30 02:58:26 +0300

    game list page created

--- pages/list.js
+++ pages/list.js
@@ -1,26 +1,56 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head'
 import { useAuth } from '../contexts/auth';
 import { Container } from '../components';
 
 const ListPage = () => {
   const auth = useAuth();
+  const [games, setGames] = useState([])
 
-  console.log(auth)
-  return (
+  const getGames = async () => {
+    try {
+      const response = await fetch(
+        'http://localhost:3001/games'
+      );
+      const games = await response.json()
+      if (games.error) {
+        console.log(games.error)
+      } else {
+        console.log(games)
+        setGames(games)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    getGames()
+  }, [])
+
+  return auth ? (
     <div className="container">
       <Head>
         <title>Games</title>
       </Head>
       <Container>
-        <img src={`/avatar/${auth && auth.username}.jpg`} />
+        <img src={auth.avatar} />
         {auth.name} {auth.event}
         <hr />
         <h3>Games</h3>
         <hr />
-        asdsd
+        <table>
+          {games.map(game => (
+            <tr>
+              <td><img src={game.icon} alt={game.name}/></td>
+              <td><h2>{game.name}</h2><p>{game.description}</p></td>
+              <td>{game.code}</td>
+            </tr>
+          ))}
+        </table>
       </Container>
     </div>
-  )
+  ) : null
 }
 
 export default ListPage
